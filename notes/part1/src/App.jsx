85d179766502b691af503d95b03adf1bd1e10063
Iria Vidal <iriavidal96@gmail.com>
2025-02-23 16:39:59 +0100

    notes: fragments

--- notes/part1/src/App.jsx
+++ notes/part1/src/App.jsx
@@ -2,12 +2,11 @@ const Hello = (props) => {
 
   console.log(props)
   return (
-    <div>
+    <>
       <p>
-
         Hello {props.name}, you are {props.age} years old
       </p>
-    </div>
+    </>
   )
 }
 
@@ -17,12 +16,12 @@ const App = () => {
   const age = 10
 
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
 
       <Hello name='Maya' age={26 + 10} />
       <Hello name={name} age={age} />
-    </div>
+    </>
   )
 }
 
