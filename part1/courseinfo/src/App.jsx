1d1ead6e68f4f19abb46b692e9c70656d1fae8ab
Iria Vidal <iriavidal96@gmail.com>
2025-02-24 10:34:52 +0100

    refactor: split App component into Header, Content, and Total

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,27 +1,55 @@
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+const Header = (props) => {
+  console.log(`Header: ${props.course}`);
 
   return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
-}
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+};
 
-export default App
+const Content = (props) => {
+  console.log(`${props.part}: ${props.exercises}`);
+
+  return (
+    <>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </>
+  );
+};
+
+const Total = (props) => {
+  console.log(props.total);
+
+  return (
+    <>
+      <p>Number of exercises {props.total}</p>
+    </>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <>
+      <Header course={course}></Header>
+
+      <Content part={part1} exercises={exercises1}></Content>
+      <Content part={part2} exercises={exercises2}></Content>
+      <Content part={part3} exercises={exercises3}></Content>
+
+      <Total total={exercises1 + exercises2 + exercises3}></Total>
+    </>
+  );
+};
+
+export default App;
