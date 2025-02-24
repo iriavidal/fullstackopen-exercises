4f1c3b80be9fa9b529d56669b6b2b4b67fe97d05
Iria Vidal <iriavidal96@gmail.com>
2025-02-24 22:33:31 +0100

    notes: complex state

--- notes/part1/src/App.jsx
+++ notes/part1/src/App.jsx
@@ -1,35 +1,24 @@
 import { useState } from "react";
 
-const Display = ({ counter }) => <div>{counter}</div>;
-
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
-
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  console.log("rendering with counter value", counter);
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0,
+  });
 
-  const increaseByOne = () => {
-    console.log("increasing, value before", counter);
-    setCounter(counter + 1);
-  };
+  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 });
 
-  const decreaseByOne = () => {
-    console.log("decreasing, value before", counter);
-    setCounter(counter - 1);
-  };
+  const handleRightClick = () =>
+    setClicks({ ...clicks, right: clicks.right + 1 });
 
-  const setToZero = () => {
-    console.log("resetting to zero, value before", counter);
-    setCounter(0);
-  };
+  /* It is forbidden in React to mutate state directly, since it can result in unexpected side effects. Changing state has to always be done by setting the state to a new object. */
 
   return (
     <div>
-      <Display counter={counter} />
-
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   );
 };
