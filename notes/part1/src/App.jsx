31db923f40249d1644caecbc5e43eedafc84f29c
Iria Vidal <iriavidal96@gmail.com>
2025-02-24 20:49:17 +0100

    notes: component helper functions

--- notes/part1/src/App.jsx
+++ notes/part1/src/App.jsx
@@ -1,28 +1,34 @@
 const Hello = (props) => {
+  console.log(props);
 
-  console.log(props)
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  };
+
   return (
-    <>
+    <div>
       <p>
         Hello {props.name}, you are {props.age} years old
       </p>
-    </>
-  )
-}
+
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  );
+};
 
 const App = () => {
-
-  const name = 'Peter'
-  const age = 10
+  const name = "Peter";
+  const age = 10;
 
   return (
     <>
       <h1>Greetings</h1>
 
-      <Hello name='Maya' age={26 + 10} />
+      <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
