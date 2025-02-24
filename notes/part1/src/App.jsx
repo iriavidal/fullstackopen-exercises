13ca9b10226872ae41c635ed115d8b0705726bff
Iria Vidal <iriavidal96@gmail.com>
2025-02-24 20:58:06 +0100

    notes: page re-rendering

--- notes/part1/src/App.jsx
+++ notes/part1/src/App.jsx
@@ -1,47 +1,6 @@
-/* const Hello = (props) => {
-  console.log(props);
-
-  //const name = props.name;
-  //const age = props.age;
-  const { name, age } = props;
-
-  const bornYear = () => new Date().getFullYear() - age;
-
-  return (
-    <>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </>
-  );
-}; */
-
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
-
-  return (
-    <>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </>
-  );
-};
-
-const App = () => {
-  const name = "Peter";
-  const age = 10;
-
-  return (
-    <>
-      <h1>Greetings</h1>
-
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </>
-  );
+const App = (props) => {
+  const { counter } = props;
+  return <div>{counter}</div>;
 };
 
 export default App;
