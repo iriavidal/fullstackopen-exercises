0dd6e91d66765f0e7c84d01965e3db0282517445
Iria Vidal <iriavidal96@gmail.com>
2025-02-25 10:24:03 +0100

    notes: passing Event Handlers to Child Components

--- notes/part1/src/App.jsx
+++ notes/part1/src/App.jsx
@@ -1,67 +1,8 @@
 import { useState } from "react";
 
-/* const App1 = () => {
-  const [value, setValue] = useState(10);
-
-  const hello = () => {
-    const handler = () => console.log("hello world");
-    return handler;
-  };
-
-  return (
-    <div>
-      {value}
-      <button onClick={hello()}>button</button>
-      // <button onClick={() => console.log("hello world")}>button</button>
-    </div>
-  );
-}; */
-
-const App2 = () => {
-  const [value, setValue] = useState(10);
-
-  /* const hello = (who) => {
-    const handler = () => {
-      console.log("hello", who);
-    };
-    return handler;
-  }; */
-
-  const hello = (who) => () => {
-    console.log("hello", who);
-  };
-
-  /* Functions returning functions can be utilized in defining generic functionality that can be customized with parameters. The hello function that creates the event handlers can be thought of as a factory that produces customized event handlers meant for greeting users. */
-
-  return (
-    <div>
-      {value}
-
-      <button onClick={hello("world")}>button</button>
-      <button onClick={hello("react")}>button</button>
-      <button onClick={hello("function")}>button</button>
-    </div>
-  );
-};
-
-/* const App = () => {
-  const [value, setValue] = useState(10);
-
-  const setToValue = (newValue) => () => {
-    console.log("value now", newValue);
-    setValue(newValue);
-  };
-
-  return (
-    <div>
-      {value}
-
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>reset</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
-    </div>
-  );
-}; */
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
 const App = () => {
   const [value, setValue] = useState(10);
@@ -74,13 +15,11 @@ const App = () => {
   return (
     <div>
       {value}
-      <button onClick={() => setToValue(1000)}>thousand</button>
-      <button onClick={() => setToValue(0)}>reset</button>
-      <button onClick={() => setToValue(value + 1)}>increment</button>
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   );
 };
-
-/* Choosing between the two presented ways of defining your event handlers is mostly a matter of taste. */
 
 export default App;
