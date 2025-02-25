a2491cbba901117401d9a5ccb9080fdc7d6411fd
Iria Vidal <iriavidal96@gmail.com>
2025-02-25 10:36:34 +0100

    notes: do Not Define Components Within Components

--- notes/part1/src/App.jsx
+++ notes/part1/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+/* Never define components inside of other components. The method provides no benefits and leads to many unpleasant problems. The biggest problems are because React treats a component defined inside of another component as a new component in every render. This makes it impossible for React to optimize the component. */
+
+const Display = (props) => <div>{props.value}</div>;
+
 const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 );
@@ -14,7 +18,7 @@ const App = () => {
 
   return (
     <div>
-      {value}
+      <Display value={value} />
       <Button handleClick={() => setToValue(1000)} text="thousand" />
       <Button handleClick={() => setToValue(0)} text="reset" />
       <Button handleClick={() => setToValue(value + 1)} text="increment" />
