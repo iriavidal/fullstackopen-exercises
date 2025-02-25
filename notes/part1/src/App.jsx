bdafc3af429531d8e214213dc6eb19fe60844af8
Iria Vidal <iriavidal96@gmail.com>
2025-02-25 09:40:37 +0100

    notes: event Handling Revisited

--- notes/part1/src/App.jsx
+++ notes/part1/src/App.jsx
@@ -1,48 +1,33 @@
 import { useState } from "react";
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return <div>the app is used by pressing the buttons</div>;
-  }
-  return <div>button press history: {props.allClicks.join(" ")}</div>;
-};
+/* const App = () => {
+  const [value, setValue] = useState(10);
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
-);
+  // Event handlers must always be a function or a reference to a function. The button will not work if the event handler is set to a variable of any other type.
+
+  return (
+    <div>
+      {value}
+      {// <button onClick={() => console.log("clicked the button")}>button</button>}
+
+      <button onClick={() => setValue(0)}>button</button>{" "} // Defining event handlers directly in the attribute of the button is not necessarily the best possible idea.
+      
+    </div>
+  );
+}; */
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
+  const [value, setValue] = useState(10);
 
-  const [total, setTotal] = useState(0);
-
-  /* The useState function (as well as the useEffect function) must not be called from inside of a loop, a conditional expression, or any place that is not a function defining a component. This must be done to ensure that the hooks are always called in the same order, and if this isn't the case the application will behave erratically. */
-
-  /* To recap, hooks may only be called from the inside of a function body that defines a React component. */
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    const updatedLeft = left + 1;
-    setLeft(updatedLeft);
-    setTotal(updatedLeft + right);
-  };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    const updatedRight = right + 1;
-    setRight(updatedRight);
-    setTotal(updatedRight + left);
+  const handleClick = () => {
+    console.log("clicked the button");
+    setValue(0);
   };
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text="left" />
-      <Button handleClick={handleRightClick} text="right" />
-      {right}
-      <History allClicks={allClicks} />
+      {value}
+      <button onClick={handleClick}>button</button>
     </div>
   );
 };
