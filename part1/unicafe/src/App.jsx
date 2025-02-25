249cabba40f484fb5b23b96f6b53e12465a67162
Iria Vidal <iriavidal96@gmail.com>
2025-02-25 11:16:49 +0100

    feat: add initial feedback tracking for Unicafe
    - Implement feedback buttons for "good", "neutral", and "bad"
    - Store feedback counts using React state
    - Display total counts for each category
    - Data persists only during the current session

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,42 @@
 import { useState } from "react";
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const handleGoodClick = () => {
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+  };
+
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+  };
+
+  const handleBadClick = () => {
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+  };
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text="good"></Button>
+      <Button handleClick={handleNeutralClick} text="neutral"></Button>
+      <Button handleClick={handleBadClick} text="bad"></Button>
+
+      <h1>statictics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  );
 };
 
 export default App;
