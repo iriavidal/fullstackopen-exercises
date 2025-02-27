756eb156d04e203d4773a61da88b79ee22274852
Iria Vidal <iriavidal96@gmail.com>
2025-02-27 20:24:26 +0100

    notes: filtering Displayed Elements

--- notes/part2/src/App.jsx
+++ notes/part2/src/App.jsx
@@ -4,6 +4,7 @@ import Note from "./components/Note";
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
   const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
 
   const addNote = (event) => {
     event.preventDefault();
@@ -20,15 +21,22 @@ const App = (props) => {
   const handleNoteChange = (event) => {
     console.log(event.target.value);
     setNewNote(event.target.value);
-
-    /* Note that we did not need to call the event.preventDefault() method like we did in the onSubmit event handler. This is because no default action occurs on an input change, unlike a form submission. */
   };
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
 
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
