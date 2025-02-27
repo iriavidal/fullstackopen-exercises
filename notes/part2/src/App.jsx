3d47fddb3a6a3fd41f60bb9bddac7fe3bf313ca4
Iria Vidal <iriavidal96@gmail.com>
2025-02-27 20:15:25 +0100

    notes: controlled component

--- notes/part2/src/App.jsx
+++ notes/part2/src/App.jsx
@@ -3,10 +3,25 @@ import Note from "./components/Note";
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
 
   const addNote = (event) => {
     event.preventDefault();
-    console.log("button clicked", event.target);
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+
+    /* Note that we did not need to call the event.preventDefault() method like we did in the onSubmit event handler. This is because no default action occurs on an input change, unlike a form submission. */
   };
 
   return (
@@ -19,7 +34,7 @@ const App = (props) => {
       </ul>
 
       <form onSubmit={addNote}>
-        <input />
+        <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
     </div>
