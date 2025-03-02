c982620301e9e398a7e6da92dabbc02ceb48e47f
Iria Vidal <iriavidal96@gmail.com>
2025-03-02 08:59:09 +0100

    feat: add phone number field to phonebook form
    - Added a new input field for phone numbers in the phonebook form
    - Updated the personObject to include the phone property
    - Implemented validation to prevent adding empty names or phone numbers
    - Refactored and cleaned up the previous addPerson function for better readability and efficiency

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,24 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "936487634" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
 
   const addPerson = (e) => {
     e.preventDefault();
 
-    const personObject = {
-      name: newName,
-    };
+    if (!newName.trim()) return alert("Name is required");
+    if (!newPhone.trim()) return alert("Phone is required");
 
-    //console.log(personObject);
-    //console.log(persons.findIndex((el) => el.name === personObject.name));
+    const index = persons.findIndex((el) => el.name === newName);
+    if (index !== -1) return alert(`${newName} is already added to phonebook`);
 
-    const index = persons.findIndex((el) => el.name === personObject.name);
-    if (index != -1) {
-      alert(`${personObject.name} is already added to phonebook`);
-    } else {
-      setPersons(persons.concat(personObject));
-      setNewName("");
-    }
+    setPersons([...persons, { name: newName, phone: newPhone }]);
+    setNewName("");
+    setNewPhone("");
   };
 
   const handlePersonChange = (e) => {
@@ -28,13 +26,22 @@ const App = () => {
     setNewName(e.target.value);
   };
 
+  const handlePhoneChange = (e) => {
+    //console.log(e.target.value);
+    setNewPhone(e.target.value);
+  };
+
   return (
     <>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name:{" "}
+          name:
           <input type="text" value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          number:{" "}
+          <input type="tel" value={newPhone} onChange={handlePhoneChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -43,7 +50,9 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person, index) => (
-          <li key={index}>{person.name}</li>
+          <li key={index}>
+            {person.name} ({person.phone})
+          </li>
         ))}
       </ul>
     </>
