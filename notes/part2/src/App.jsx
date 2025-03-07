5df0d38d41d4a6bd5db82d417b4347f21d088d03
Iria Vidal <iriavidal96@gmail.com>
2025-03-07 10:04:27 +0100

    notes: changing the Importance of Notes

--- notes/part2/src/App.jsx
+++ notes/part2/src/App.jsx
@@ -36,6 +36,24 @@ const App = () => {
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
+  /* const toggleImportanceOf = (id) => {
+    console.log(`importance of ${id} needs to be toggled`);
+  }; */
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+    /* we created a new note bc we must never mutate state directly in React */
+    /* the new object changedNote is only a so-called shallow copy, meaning that the values of the new object are the same as the values of the old object */
+
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
+      /* The callback function sets the component's notes state to a new array that contains all the items from the previous notes array, except for the old note which is replaced by the updated version of it returned by the server */
+    });
+    /* PUT replaces the note */
+  };
+
   return (
     <div>
       <h1>Notes</h1>
@@ -46,7 +64,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
