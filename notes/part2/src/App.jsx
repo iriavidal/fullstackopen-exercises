ff9118ac0290a10a24941d4789be0bc54b701760
Iria Vidal <iriavidal96@gmail.com>
2025-03-07 09:32:56 +0100

    notes: sending Data to the Server

--- notes/part2/src/App.jsx
+++ notes/part2/src/App.jsx
@@ -7,45 +7,54 @@ const App = () => {
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
-  /* useEffect(() => {
+  useEffect(() => {
     console.log("effect");
     axios.get("http://localhost:3001/notes").then((response) => {
       console.log("promise fulfilled");
       setNotes(response.data);
     });
   }, []);
-  console.log("render", notes.length, "notes"); */
+  console.log("render", notes.length, "notes");
 
-  /* const hook = () => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((response) => {
-      console.log("promise fulfilled");
-      setNotes(response.data);
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: String(notes.length + 1),
+    };
+
+    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
+      setNotes(notes.concat(response.data));
+      setNewNote("");
     });
   };
 
-  useEffect(hook, []);
-  console.log("render", notes.length, "notes"); */
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
 
-  /* By default, effects run after every completed render, but you can choose to fire it only when certain values have changed. */
-  /* So by default, the effect is always run after the component has been rendered. In our case, however, we only want to execute the effect along with the first render. */
-  /* The second parameter of useEffect is used to specify how often the effect is run. If the second parameter is an empty array [], then the effect is only run along with the first render of the component. */
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
-  useEffect(() => {
-    console.log("effect");
-
-    const eventHandler = (response) => {
-      console.log("promise fulfilled");
-      setNotes(response.data);
-    };
-
-    const promise = axios.get("http://localhost:3001/notes");
-    promise.then(eventHandler);
-  }, []);
-
-  /* A reference to an event handler function is assigned to the variable eventHandler. The promise returned by the get method of Axios is stored in the variable promise. The registration of the callback happens by giving the eventHandler variable, referring to the event-handler function, as a parameter to the then method of the promise. It isn't usually necessary to assign functions and promises to variables, and a more compact way of representing things, as seen below, is sufficient. */
-
-  return <></>;
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
