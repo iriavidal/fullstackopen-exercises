49906514764c35cac4805cb0b723d15157b52dd1
Iria Vidal <iriavidal96@gmail.com>
2025-03-23 10:52:02 +0100

    notes: web and Express

--- notes/part3/index.js
+++ notes/part3/index.js
@@ -1,14 +1,5 @@
-// Importamos el módulo de CommonJS "http" de Node.js para poder crear un servidor
-const http = require("http");
-
-/* // Creamos el servidor con "http.createServer()" y definimos una función callback que maneja cada solicitud entrante
-const app = http.createServer((request, response) => {
-  // Configuramos la cabecera de la respuesta con un código de estado 200 (OK) y especificamos que el contenido será de tipo texto plano
-  response.writeHead(200, { "Content-Type": "text/plain" });
-
-  // Enviamos la respuesta con el mensaje "Hello World" y finalizamos la respuesta
-  response.end("Hello World");
-}); */
+const express = require("express");
+const app = express();
 
 let notes = [
   {
@@ -28,16 +19,15 @@ let notes = [
   },
 ];
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(notes)); // Transforma el JSON a string
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>"); // Establece automáticamente la cabecera "Content-Type text/html"
 });
 
-// Definimos el puerto en el que se ejecutará el servidor
+app.get("/api/notes", (request, response) => {
+  response.json(notes); // Establece automáticamente la cabecera "Content-Type application/json"
+});
+
 const PORT = 3001;
-
-// Hacemos que el servidor escuche en el puerto definido
-app.listen(PORT);
-
-// Mostramos un mensaje en la consola indicando que el servidor está en funcionamiento
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
