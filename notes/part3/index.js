ae1a7203b689bca6d02fe06f9321a3f9a323e98b
Iria Vidal <iriavidal96@gmail.com>
2025-03-23 16:47:28 +0100

    notes: getting a single resource

--- notes/part3/index.js
+++ notes/part3/index.js
@@ -27,6 +27,27 @@ app.get("/api/notes", (request, response) => {
   response.json(notes); // Establece automáticamente la cabecera "Content-Type application/json"
 });
 
+// Obtener una sola nota a partir de su id
+app.get("/api/notes/:id", (request, response) => {
+  /* const id = request.params.id;
+  const note = notes.find((note) => {
+    console.log(note.id, typeof note.id, id, typeof id, note.id === id);
+    return note.id === id;
+  });
+  console.log(note);
+  response.json(note); */
+
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+    /* Dado que no se adjuntan datos a la respuesta, utilizamos el método status para establecer el estado y el método end para responder a la solicitud sin enviar ningún dato. */
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
