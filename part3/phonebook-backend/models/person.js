6ced360a7468ee1a46761fb7e8bcbfa0877ba7ee
Iria Vidal <iriavidal96@gmail.com>
2025-03-27 18:53:04 +0100

    refactor: comment database connection from person.js to avoid redundancy

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 
-const url = process.env.MONGODB_URI;
+/* const url = process.env.MONGODB_URI;
 
 console.log("connecting to", url);
 
@@ -13,7 +13,7 @@ mongoose
   })
   .catch((error) => {
     console.log("error connecting to MongoDB:", error.message);
-  });
+  }); */
 
 const personSchema = new mongoose.Schema({
   name: String,
