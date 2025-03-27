c070a545ef4d444914a381b6becfa6441fd43a26
Iria Vidal <iriavidal96@gmail.com>
2025-03-27 19:04:38 +0100

    UI updated and built

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -83,6 +83,23 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 }); */
 
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
