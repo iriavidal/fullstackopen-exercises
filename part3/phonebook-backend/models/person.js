e7feb2c34d7dbf9746634dfa1c1e1276fd7235ce
Iria Vidal <iriavidal96@gmail.com>
2025-04-12 11:50:42 +0200

    feat: add custom phone number validation to ensure proper format and length

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -24,6 +24,12 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
+    validate: {
+      validator: function (v) {
+        return /^(\d{2,3})-\d+$/.test(v) && v.length >= 8;
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
     required: [true, "Number is required"],
   },
 });
