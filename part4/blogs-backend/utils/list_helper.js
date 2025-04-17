fb1f74d8c3feebef2315b7daafc16bfd641b325d
Iria Vidal <iriavidal96@gmail.com>
2025-04-17 11:28:33 +0200

    test: add totalLikes function and corresponding unit tests

--- part4/blogs-backend/utils/list_helper.js
+++ part4/blogs-backend/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const totalLikes = blogs.reduce((sum, blog) => sum + blog.likes, 0);
+  console.log(totalLikes);
+  return totalLikes;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
