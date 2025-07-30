2125d36ec7e15da9ec1a67bf551b1d09eb7d7486
Iria Vidal <iriavidal96@gmail.com>
2025-07-30 21:10:43 +0200

    feat: handle invalid JWT error in middleware
    Added a case to the errorHandler middleware to return a 401 response with an appropriate error message when a JsonWebTokenError occurs, improving authentication error handling.

--- notes/part4/backend-notes/utils/middleware.js
+++ notes/part4/backend-notes/utils/middleware.js
@@ -44,6 +44,8 @@ const errorHandler = (error, request, response, next) => {
     return response
       .status(400)
       .json({ error: "expected `username` to be unique" });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({ error: "token invalid" });
   }
 
   // Si el error no es de los tipos anteriores, lo pasa al siguiente manejador de errores
