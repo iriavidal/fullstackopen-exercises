e66078dc003365ab5189a35ef51a947951e38708
Iria Vidal <iriavidal96@gmail.com>
2025-08-14 21:17:45 +0200

    feat: wire like button to handleLike in Blog component
    Updates the Blog component to accept a handleLike function as a prop and assign it to the onClick event of the like button. Allows the component to trigger likes updates handled by the parent component.

--- part5/bloglist-frontend-main/src/components/Blog.jsx
+++ part5/bloglist-frontend-main/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog, handleLike }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{" "}
+        <button onClick={() => setShowDetails(!showDetails)}>
+          {showDetails ? "hide" : "view"}
+        </button>
+      </div>
+
+      {showDetails && (
+        <div>
+          <p>{blog.url}</p>
+          <p>
+            likes {blog.likes}{" "}
+            <button onClick={() => handleLike(blog)}>like</button>
+          </p>
+          <p>{blog.user?.name}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
