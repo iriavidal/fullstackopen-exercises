a75b975631b10041fa673e4aac3ad619a459c8b2
Iria Vidal <iriavidal96@gmail.com>
2025-08-14 21:47:42 +0200

    feat: sort blogs by number of likes
    Changes the display order of blogs in the frontend to be sorted by likes in descending order. Uses array sort in combination with map to render blogs in the new order.

--- part5/bloglist-frontend-main/src/App.jsx
+++ part5/bloglist-frontend-main/src/App.jsx
@@ -138,9 +138,11 @@ const App = () => {
 
           <hr />
 
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} handleLike={handleLike} />
-          ))}
+          {[...blogs]
+            .sort((a, b) => b.likes - a.likes)
+            .map((blog) => (
+              <Blog key={blog.id} blog={blog} handleLike={handleLike} />
+            ))}
           <hr />
           <button onClick={handleLogout}>log-out</button>
         </div>
