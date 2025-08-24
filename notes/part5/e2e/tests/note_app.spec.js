d84baa5233853a5732d7decaba7b5dd404284b8c
Iria Vidal <iriavidal96@gmail.com>
2025-08-24 18:50:31 +0200

    notes (test): añadir prueba e2e para crear una nueva nota
    Se añade un nuevo conjunto de pruebas con Playwright que valida el flujo de creación de notas:
    
    - El usuario inicia sesión con credenciales válidas
    - Se abre el formulario para crear una nueva nota
    - Se rellena el campo de texto y se guarda la nota
    - Se verifica que la nueva nota aparece en la lista de notas visibles en pantalla

--- notes/part5/e2e/tests/note_app.spec.js
+++ notes/part5/e2e/tests/note_app.spec.js
@@ -43,4 +43,24 @@ describe("Note app", () => {
 
     await expect(page.getByText("Iria Vidal logged in")).toBeVisible();
   });
+
+  describe("when logged in", () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole("button", { name: "log in" }).click();
+      await page.getByTestId("username").fill("iria");
+      await page.getByTestId("password").fill("1234");
+      await page.getByRole("button", { name: "login" }).click();
+    });
+
+    test("a new note can be created", async ({ page }) => {
+      await page.getByRole("button", { name: "new note" }).click();
+      await page.getByRole("textbox").fill("a note created by playwright");
+      await page.getByRole("button", { name: "save" }).click();
+      await expect(
+        page.getByText("a note created by playwright")
+      ).toBeVisible();
+    });
+  });
+
+  /* Dado que hemos evitado que las pruebas se ejecuten en paralelo, Playwright ejecuta las pruebas en el orden en que aparecen en el código de prueba. Es decir, primero se realiza la prueba user can log in, donde el usuario inicia sesión en la aplicación. Después de esto se ejecuta la prueba a new note can be created, que también realiza un inicio de sesión, en el bloque beforeEach. ¿Por qué se hace esto, no está ya el usuario conectado gracias a la prueba anterior? No, porque la ejecución de cada prueba comienza desde el "estado cero" del navegador, todos los cambios realizados en el estado del navegador por las pruebas anteriores se resetean. */
 });
