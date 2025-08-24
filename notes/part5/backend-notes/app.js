877c41e72ac81437ba39e982eef66bb7beabaea7
Iria Vidal <iriavidal96@gmail.com>
2025-08-24 18:58:51 +0200

    notes (feat): habilitar endpoint de testing solo en entorno test
    Se modifica `app.js` para que el controlador de testing solo se registre cuando NODE_ENV === "test":
    
    - Evita exponer el endpoint /api/testing/reset en otros entornos
    - Mejora la seguridad y asegura que el reseteo de la base de datos
      solo se ejecute durante pruebas automatizadas

--- notes/part5/backend-notes/app.js
+++ notes/part5/backend-notes/app.js
@@ -70,6 +70,11 @@ app.use("/api/users", usersRouter);
 
 app.use("/api/login", loginRouter);
 
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
 // Middleware para manejar peticiones a endpoints desconocidos (404)
 app.use(middleware.unknownEndpoint);
 
