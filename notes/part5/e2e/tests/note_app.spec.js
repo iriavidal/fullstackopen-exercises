cffafcfcc79f67a6b68cc9db321c63b3d65bd609
Iria Vidal <iriavidal96@gmail.com>
2025-08-25 19:14:48 +0200

    notes (refactor): usar helper de login en los tests e2e
    Se actualizaron los tests que requieren inicio de sesión para utilizar la función auxiliar de login definida en helper.js, eliminando código repetitivo.

--- notes/part5/e2e/tests/note_app.spec.js
+++ notes/part5/e2e/tests/note_app.spec.js
@@ -1,4 +1,5 @@
 const { test, describe, expect, beforeEach } = require("@playwright/test");
+const { loginWith } = require("./helper");
 
 describe("Note app", () => {
   beforeEach(async ({ page, request }) => {
@@ -14,42 +15,8 @@ describe("Note app", () => {
     await page.goto("http://localhost:5173");
   });
 
-  /* test("front page can be opened", async ({ page }) => {
-    // await page.goto("http://localhost:5173");
-
-    const locator = await page.getByText("Notes");
-    await expect(locator).toBeVisible();
-    await expect(
-      page.getByText(
-        "Note app, Department of Computer Science, University of Helsinki 2024"
-      )
-    ).toBeVisible();
-  }); */
-
-  test("login form can be opened", async ({ page }) => {
-    // npm test -- -- --ui
-
-    // await page.goto("http://localhost:5173");
-
-    await page.getByRole("button", { name: "log in" }).click();
-    // await page.getByRole("textbox").fill("mluukkai");
-
-    /* await page.getByRole("textbox").first().fill("iria");
-    await page.getByRole("textbox").last().fill("1234"); */
-
-    const textboxes = await page.getByRole("textbox").all();
-    await textboxes[0].fill("iria");
-    await textboxes[1].fill("1234");
-
-    /* Ambas versiones de la prueba funcionan. Sin embargo, ambas son problemáticas en la medida en que si el formulario de registro cambia, las pruebas pueden fallar, ya que dependen de que los campos estén en la página en un cierto orden. 
-    
-    
-    Una mejor solución es definir atributos de id de prueba únicos para los campos, y buscarlos en las pruebas utilizando el método getByTestId.*/
-
-    await page.getByTestId("username").fill("iria");
-    await page.getByTestId("password").fill("1234");
-    await page.getByRole("button", { name: "login" }).click();
-
+  test("user can log in", async ({ page }) => {
+    await loginWith(page, "iria", "1234");
     await expect(page.getByText("Iria Vidal logged in")).toBeVisible();
   });
 
@@ -73,10 +40,7 @@ describe("Note app", () => {
 
   describe("when logged in", () => {
     beforeEach(async ({ page }) => {
-      await page.getByRole("button", { name: "log in" }).click();
-      await page.getByTestId("username").fill("iria");
-      await page.getByTestId("password").fill("1234");
-      await page.getByRole("button", { name: "login" }).click();
+      await loginWith(page, "iria", "1234");
     });
 
     test("a new note can be created", async ({ page }) => {
