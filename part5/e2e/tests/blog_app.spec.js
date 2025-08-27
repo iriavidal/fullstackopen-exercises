e77ca93dfc6b3de940de9c9d576ed0f8768bc0f2
Iria Vidal <iriavidal96@gmail.com>
2025-08-27 20:16:48 +0200

    feat: add E2E tests for login functionality
    Added end-to-end tests to check that login succeeds with correct credentials and fails with wrong credentials, ensuring the login flow works as expected.

--- part5/e2e/tests/blog_app.spec.js
+++ part5/e2e/tests/blog_app.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
 
 describe("Blog app", () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post("http://localhost:3001/api/testing/reset");
+    await request.post("http://localhost:3001/api/users", {
+      data: {
+        name: "Iria Vidal",
+        username: "iria",
+        password: "1234",
+      },
+    });
+
     await page.goto("http://localhost:5173");
   });
 
@@ -10,5 +19,26 @@ describe("Blog app", () => {
     await expect(page.getByText("username")).toBeVisible();
     await expect(page.getByText("password")).toBeVisible();
     await page.getByRole("button", { name: "login" }).click();
+  });
+
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await page.getByTestId("username").fill("iria");
+      await page.getByTestId("password").fill("1234");
+      await page.getByRole("button", { name: "login" }).click();
+
+      await expect(page.getByText("Iria Vidal logged in")).toBeVisible();
+    });
+
+    test("fails with wrong credentials", async ({ page }) => {
+      await page.getByTestId("username").fill("iria");
+      await page.getByTestId("password").fill("wrong");
+      await page.getByRole("button", { name: "login" }).click();
+
+      const errorDiv = await page.locator(".error");
+      await expect(errorDiv).toContainText("Wrong credentials");
+
+      await expect(page.getByText("Iria Vidal logged in")).not.toBeVisible();
+    });
   });
 });
