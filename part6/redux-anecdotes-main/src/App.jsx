9d01e955f583ceb5278095e84719c150e841656f
Iria Vidal <iriavidal96@gmail.com>
2025-09-05 10:48:17 +0200

    feat: implement new anecdote creation form in App component
    Added form with input and submit button to create new anecdotes. Implemented addAnecdote function that dispatches NEW_ANECDOTE action with content from form input. The form resets after submission, completing the full CRUD functionality for anecdotes.

--- part6/redux-anecdotes-main/src/App.jsx
+++ part6/redux-anecdotes-main/src/App.jsx
@@ -2,33 +2,42 @@ import { useSelector, useDispatch } from "react-redux";
 
 const App = () => {
   const anecdotes = useSelector((state) => state);
-
   const dispatch = useDispatch();
 
   const vote = (id) => {
     dispatch({ type: "VOTE", payload: { id } });
   };
 
+  const addAnecdote = (event) => {
+    event.preventDefault();
+
+    const content = event.target.anecdote.value;
+
+    event.target.anecdote.value = "";
+
+    dispatch({ type: "NEW_ANECDOTE", payload: content });
+  };
+
   return (
     <div>
       <h2>Anecdotes</h2>
-      {}
       {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            {}
             <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
         </div>
       ))}
       <h2>create new</h2>
-      <form>
+      {}
+      <form onSubmit={addAnecdote}>
         <div>
-          <input />
+          {}
+          <input name="anecdote" />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
       </form>
     </div>
   );
